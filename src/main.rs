4e96db0e9b6523cb8a6a9d1cf0300d14557837ef
nozwock <57829219+nozwock@users.noreply.github.com>
2023-09-27 09:19:40 +0530

    Add basic server app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Router};
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health", get(|| async { StatusCode::OK }));
+
+    let listener = TcpListener::bind("0.0.0.0:0").unwrap();
+    let addr = listener.local_addr().unwrap().to_string();
+    println!("Running on:\nhttp://{}", addr);
+
+    axum::Server::from_tcp(listener)
+        .unwrap()
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
