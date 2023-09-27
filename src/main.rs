7d68b56fe0da48f14363e80f240bee735576a56a
nozwock <57829219+nozwock@users.noreply.github.com>
2023-09-28 02:20:11 +0530

    Impl subscribe handler

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 mod config;
 
-use anyhow::Result;
 use axum::{
-    extract::Form,
+    extract::{Form, State},
     http::StatusCode,
     routing::{get, post},
     Router,
 };
+use chrono::Utc;
 use serde::Deserialize;
+use sqlx::PgPool;
 use std::net::TcpListener;
+use uuid::Uuid;
 
 #[derive(Debug, Deserialize)]
 #[allow(dead_code)]
@@ -18,17 +20,37 @@ struct SubscriptionInfo {
 }
 
 /// Content-Type: application/x-www-form-urlencoded
-async fn subscribe(Form(info): Form<SubscriptionInfo>) {
-    eprintln!("{:?}", info);
+async fn subscribe(
+    State(poll): State<PgPool>,
+    Form(form): Form<SubscriptionInfo>,
+) -> Result<StatusCode, (StatusCode, String)> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscribers (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(&poll)
+    .await
+    .map_err(internal_error)?;
+
+    Ok(StatusCode::OK)
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> anyhow::Result<()> {
     let cfg = config::Config::load()?;
+
+    let pool = sqlx::PgPool::connect(&cfg.db.as_url(true)).await?;
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/health", get(|| async { StatusCode::OK }))
-        .route("/subscribe", post(subscribe));
+        .route("/subscribe", post(subscribe).with_state(pool));
 
     let listener = TcpListener::bind(format!("0.0.0.0:{}", cfg.server.port))?;
     let addr = listener.local_addr()?.to_string();
@@ -40,4 +62,11 @@ async fn main() -> Result<()> {
         .await?;
 
     Ok(())
+}
+
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
