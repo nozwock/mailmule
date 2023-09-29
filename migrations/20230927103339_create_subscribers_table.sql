e04df38f19b25b1732b43716854e0ce70ce4355c
nozwock <57829219+nozwock@users.noreply.github.com>
2023-09-30 02:44:06 +0530

    Add status column to the first migration script
    It's fine, since we don't have the app in production yet.

--- migrations/20230927103339_create_subscribers_table.sql
+++ migrations/20230927103339_create_subscribers_table.sql
@@ -1,8 +1,9 @@
 -- Add migration script here
-CREATE TABLE subscribers (
+CREATE TABLE subscribers(
     id uuid NOT NULL,
     PRIMARY KEY (id),
     email TEXT NOT NULL UNIQUE,
     name TEXT NOT NULL,
+    status TEXT NOT NULL,
     subscribed_at timestamptz NOT NULL
 );
